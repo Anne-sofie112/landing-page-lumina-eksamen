7ae307b2fbd8730e489f065dca445945ac9a9eab
Anne-sofie112 <eaa25asabr@students.eaaa.dk>
2025-12-01 11:03:21 +0100

    lave javascript array til produkter og rydde op i html produkter

---
+++ main.js
@@ -0,0 +1,32 @@
+"use strict";
+
+const speakerVariants = [
+  {
+    className: "green",
+    image: "img/grøn.png",
+    name: "Sage Green",
+    price: "1999,-",
+    alt: "Højtaler i Sage Green",
+  },
+  {
+    className: "purple",
+    image: "img/lilla.png",
+    name: "Lavender Mist",
+    price: "1999,-",
+    alt: "Højtaler i Lavender Mist",
+  },
+  {
+    className: "white",
+    image: "img/hvid.png",
+    name: "Moonlight White",
+    price: "1999,-",
+    alt: "Højtaler i Moonlight White",
+  },
+  {
+    className: "rose",
+    image: "img/lyserød.png",
+    name: "Dusty Rose",
+    price: "1999,-",
+    alt: "Højtaler i Dusty Rose",
+  },
+];
